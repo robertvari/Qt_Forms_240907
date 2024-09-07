4185772209fba4c26c4b6a331bff707b39924ae6
Robert Vari <mail.robertvari@gmail.com>
2024-09-07 18:02:12 +0200

    update

--- SimpleForm.py
+++ SimpleForm.py
@@ -1,4 +1,7 @@
-from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtWidgets import (
+    QWidget, QApplication, QLabel, QLineEdit, 
+    QPushButton, QHBoxLayout, QVBoxLayout
+    )
 import sys
 
 # Subclass QWidget to define our own window
@@ -6,4 +9,23 @@ class MyForm(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Registration Form")
-        self.showMaximized(True)
+        # self.showMaximized()
+
+        # Create layouts for our widgets
+        main_layout = QVBoxLayout(self)
+
+        name_layout = QHBoxLayout()
+        main_layout.addLayout(name_layout)
+
+        email_layout = QHBoxLayout()
+        main_layout.addLayout(email_layout)
+
+        address_layout = QHBoxLayout()
+        main_layout.addLayout(address_layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MyForm()
+    win.show()
+    app.exec()
