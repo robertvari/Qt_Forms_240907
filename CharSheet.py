3243880c45b6ed37a5c4b18b06019414745a97d2
Robert Vari <mail.robertvari@gmail.com>
2024-09-08 15:30:43 +0200

    Update

--- CharSheet.py
+++ CharSheet.py
@@ -1,5 +1,8 @@
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout
 import sys
+from char_sheet_lib.PlayerAttributes import PlayerAttributes
+from char_sheet_lib.PlayerDetails import PlayerDetails
+from char_sheet_lib.Proficiencies import Proficiencies
 
 class CharSheet(QWidget):
     def __init__(self):
@@ -7,6 +10,17 @@ class CharSheet(QWidget):
         self.setWindowTitle("D&D Character Sheet")
 
         main_layout = QVBoxLayout(self)
+
+        player_details = PlayerDetails()
+        main_layout.addWidget(player_details)
+
+        
+        player_attributes = PlayerAttributes()
+        main_layout.addWidget(player_attributes)
+
+
+        proficiencies = Proficiencies()
+        main_layout.addWidget(proficiencies)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- char_sheet_lib/PlayerAttributes.py
+++ char_sheet_lib/PlayerAttributes.py
@@ -4,5 +4,5 @@ class PlayerAttributes(QWidget):
     def __init__(self):
         super().__init__()
         main_layout = QVBoxLayout(self)
-        label = QLabel("Player attributes section")
+        label = QLabel("This is the player attributes section")
         main_layout.addWidget(label)
